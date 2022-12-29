2a4d52eff39f70a809d8e104e680eec5069a9930
eric_lebail <eric_lebail@hotmail.com>
2022-12-29 09:58:54 +0100

    Update version for dependencies and add python 3.10 build

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 
 test_deps = [
-    "pytest==6.2.2",
-    "pytest-cov==2.11.1",
-    "pytest-mock==1.10.0",
-    "testfixtures==6.3.0",
-    "coverage==5.5"
+    "pytest==7.2.0",
+    "pytest-cov==4.0.0",
+    "pytest-mock==3.10.0",
+    "testfixtures==7.0.4",
+    "coverage==7.0.1"
 ]
 extras = {
     'test': test_deps,
@@ -29,12 +29,12 @@ setup(
         "Operating System :: OS Independent"],
     py_modules=["tap_test_data_generator"],
     install_requires=[
-        "singer-python==5.12.1",
+        "singer-python==5.13.0",
         "setuptools==65.6.3",
         "importlib-metadata==5.2.0",
         "requests",
         "jsonschema==2.6.0",
-        "Faker==7.0.1",
+        "Faker==15.3.4",
         "exrex==0.10.5",
         "ijson==3.1.4",
         "allpairspy==2.5.0"
